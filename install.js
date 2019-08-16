5c83bf65b36b2031ec8402c23cb87330f499b78d
徐晓刚 <xxg209495@alibaba-inc.com>
2019-08-16 11:55:05 +0800

    update format

--- install.js
+++ install.js
@@ -1,45 +1,45 @@
-var fs = require('fs');
-var os = require('os');
-var spawn = require('child_process').spawn;
+const fs = require('fs');
+const os = require('os');
+const spawn = require('child_process').spawn;
 
-var base_dir = __dirname;
-var pngdefry_src_path = base_dir + "/pngdefry-master";
+const base_dir = __dirname;
+const pngdefry_src_path = base_dir + "/pngdefry-master";
 //Everything seems ok.
-//Rename pngdefry path 
+//Rename pngdefry path
 //Let's configure the tool
 try {
-    process.chdir(pngdefry_src_path);
+  process.chdir(pngdefry_src_path);
 } catch (err) {
-    process.exit();
+  process.exit();
 }
-var pngdefry_configure_file = "./configure";
-var params = [pngdefry_configure_file];
+const pngdefry_configure_file = "./configure";
+const params = [pngdefry_configure_file];
 var pngdefry_config = spawn('sh', params);
 
 pngdefry_config.on('exit', function (code, signal) {
-    if (code != null) {
+  if (code != null) {
 
-        var pngdefry_make = spawn('make');
+    var pngdefry_make = spawn('make');
 
-        pngdefry_make.on('exit', function (code, signal) {
-            if (code != null) {
-                var pngdefry_make_install = spawn('make', ['install']);
-                pngdefry_make_install.on('exit', function (code, signal) {
-                    
-                });
-                pngdefry_make_install.on("error", function (err) {
-                    console.log(err)
-                });
-            }
+    pngdefry_make.on('exit', function (code, signal) {
+      if (code != null) {
+        var pngdefry_make_install = spawn('make', ['install']);
+        pngdefry_make_install.on('exit', function (code, signal) {
+
         });
-        pngdefry_make.on("error", function (err) {
-            console.log(err)
+        pngdefry_make_install.on("error", function (err) {
+          console.log(err)
         });
-    }
+      }
+    });
+    pngdefry_make.on("error", function (err) {
+      console.log(err)
+    });
+  }
 });
 
 pngdefry_config.on("error", function (err) {
-    console.log(err);
+  console.log(err);
 });
 
 //   process.exit();
